4d55831274a519cd982a09219ab2b9b3a32d60db
Tim White <fignewtonfood@gmail.com>
2015-08-14 10:01:41 -0700

    add test for third spec

--- tests/CountRepeatsTest.php
+++ tests/CountRepeatsTest.php
@@ -2,7 +2,7 @@
     require_once "src/CountRepeats.php";
     class CountRepeatsTest extends PHPUnit_Framework_TestCase
     {
-        function test_one_letter_indentical()
+        function test_one_letter_each_indentical()
         {
             //Arrange
             $test_CountRepeats = new CountRepeats;
@@ -14,7 +14,7 @@
             $this->assertEquals(1, $result);
         }
 
-        function test_one_letter_different()
+        function test_one_letter_each_different()
         {
             //Arrange
             $test_CountRepeats = new CountRepeats;
@@ -24,6 +24,18 @@
             $result = $test_CountRepeats->countRepeatsMethod($input1, $input2);
             //Assert
             $this->assertEquals(0, $result);
+        }
+
+        function test_one_letter_to_two_word()
+        {
+            //Arrange
+            $test_CountRepeats = new CountRepeats;
+            $input1 = 'a';
+            $input2 = 'a b';
+            //Act
+            $result = $test_CountRepeats->countRepeatsMethod($input1, $input2);
+            //Assert
+            $this->assertEquals(1, $result);
         }
     }
 ?>
