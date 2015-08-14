9aa60356ebdedea892ca682ea6b905760e002871
Tim White <fignewtonfood@gmail.com>
2015-08-14 10:13:13 -0700

    add to method to pass seventh spec

--- src/CountRepeats.php
+++ src/CountRepeats.php
@@ -4,11 +4,12 @@ class CountRepeats
     function countRepeatsMethod($master_string, $search_string)
     {
         $exploded_search_array = explode(' ', $search_string);
-        if(in_array($master_string, $exploded_search_array)) {
-            return 1;
-        } else {
-            return 0;
-        }
+        $number_of_matches = 0;
+        foreach ($exploded_search_array as $word)
+            if($word == $master_string) {
+                $number_of_matches++;
+            }
+        return $number_of_matches;
     }
 }
 ?>
