b394e261ec65ff70f38778346c2901d4199a39b5
Tim White <fignewtonfood@gmail.com>
2015-08-14 10:33:34 -0700

    add to method to pass eleventh spec

--- src/CountRepeats.php
+++ src/CountRepeats.php
@@ -3,12 +3,15 @@ class CountRepeats
 {
     function countRepeatsMethod($master_string, $search_string)
     {
+        $master_string = strtolower($master_string);
         $exploded_search_array = explode(' ', $search_string);
         $number_of_matches = 0;
-        foreach ($exploded_search_array as $word)
+        foreach ($exploded_search_array as $word) {
+            $word = strtolower($word);
             if($word == $master_string) {
                 $number_of_matches++;
             }
+        }
         return $number_of_matches;
     }
 }
