e2a6daf865ae0d7dd6075730bcbe10750401f68c
Tim White <fignewtonfood@gmail.com>
2015-08-14 10:48:50 -0700

    add comments to method

--- src/CountRepeats.php
+++ src/CountRepeats.php
@@ -3,17 +3,24 @@ class CountRepeats
 {
     function countRepeatsMethod($master_string, $search_string)
     {
+      //using a regular expression, ignoring all alphanumeric characters and spaces, replaces all other characters with nothing
         $clean_search_string = preg_replace('/[^A-Za-z0-9\s]/', '', $search_string);
-        echo '/  '.$clean_search_string.'  /';
+      //first input to lowercase
         $master_string = strtolower($master_string);
+      //explodes the cleaned search into an array
         $exploded_search_array = explode(' ', $clean_search_string);
+      //creates match counter
         $number_of_matches = 0;
+      //cylcling through each word in the exploded word array
         foreach ($exploded_search_array as $word) {
+          //transforming each word to lowercase
             $word = strtolower($word);
+          //incrementing the counter if that word in the array matches the initial input
             if($word == $master_string) {
                 $number_of_matches++;
             }
         }
+      //returns the number of matches
         return $number_of_matches;
     }
 }
