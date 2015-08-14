693c0736163692af1361b1ff877d75651c30760a
Tim White <fignewtonfood@gmail.com>
2015-08-14 10:04:27 -0700

    add to method to pass third spec

--- src/CountRepeats.php
+++ src/CountRepeats.php
@@ -3,7 +3,8 @@ class CountRepeats
 {
     function countRepeatsMethod($master_string, $search_string)
     {
-        if($master_string == $search_string) {
+        $exploded_search_array = explode(' ', $search_string);
+        if(in_array($master_string, $exploded_search_array)) {
             return 1;
         } else {
             return 0;
