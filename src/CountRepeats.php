be13dc1e82c58c2277720e1b18bfdecbc51c046d
Tim White <fignewtonfood@gmail.com>
2015-08-14 09:57:29 -0700

    create method to pass first spec

--- src/CountRepeats.php
+++ src/CountRepeats.php
@@ -3,7 +3,9 @@ class CountRepeats
 {
     function countRepeatsMethod($master_string, $search_string)
     {
-
+        if($master_string == $search_string) {
+            return 1;
+        }
     }
 }
 ?>
